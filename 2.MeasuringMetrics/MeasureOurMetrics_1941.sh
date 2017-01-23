c1974ec9662681672f4b7c0e62f3c43eb5fede33
Isa Guclu <isaguclu@outlook.com>
2017-01-23 10:45:50 +0000

    Experimental Setup

--- 2.MeasuringMetrics/MeasureOurMetrics_1941.sh
+++ 2.MeasuringMetrics/MeasureOurMetrics_1941.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 folder="/home/ktgroup/Desktop/isa/Datasets/1941/"
 newFileName="OurMetrics.csv"
+java -Xss1g -Xms2g -Xmx10g -cp OWL2Predictions.jar sid.owl2predictions.utils.DirectoryStatistics $folder
 java -Xss1g -Xms2g -Xmx10g -cp OWL2Predictions.jar sid.owl2predictions.utils.ComplexityMetricsDirectoryCalculator $folder $newFileName
--- 2.MeasuringMetrics/MeasureOurMetrics_3858.sh
+++ 2.MeasuringMetrics/MeasureOurMetrics_3858.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 folder="/home/ktgroup/Desktop/isa/Datasets/3858/"
 newFileName="OurMetrics.csv"
+java -Xss1g -Xms2g -Xmx10g -cp OWL2Predictions.jar sid.owl2predictions.utils.DirectoryStatistics $folder
 java -Xss1g -Xms2g -Xmx10g -cp OWL2Predictions.jar sid.owl2predictions.utils.ComplexityMetricsDirectoryCalculator $folder $newFileName
